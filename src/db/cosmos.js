00c993d6db60577bd40ecdb8a95583dba3067859
anishkarthick <87223546+anishkarthick@users.noreply.github.com>
2022-01-14 00:31:09 +0530

    db

--- src/db/cosmos.js
+++ src/db/cosmos.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 //db
-mongoose.connect("mongodb://d6d9f436-0ee0-4-231-b9ee.mongo.cosmos.azure.com:10255/d6d9f436-0ee0-4-231-b9ee?ssl=true&replicaSet=globaldb", {
+mongoose.connect("mongodb://598e8b80-0ee0-4-231-b9ee.mongo.cosmos.azure.com:10255/598e8b80-0ee0-4-231-b9ee?ssl=true&replicaSet=globaldb", {
   auth: {
-    username: "d6d9f436-0ee0-4-231-b9ee",
-    password: "s5NpP63uXjKPXtm2OCWRurEjLGk95M87YWNrUjhWSnVQpE0CV0uuXas5ZYDrb00ZR8vsxkpDfrKjJNOGkEWPlw=="
+    username: "598e8b80-0ee0-4-231-b9ee",
+    password:"9BDvrPWjkAA7OMFOGS8ZF73GCYI5Mv8Nmabkx0UjSDhNmvNmTvrxvOdlECDLv8hcJCZL3vsnpqricOrmMWFURA=="
   },
   useNewUrlParser: true,
   useUnifiedTopology: true,
